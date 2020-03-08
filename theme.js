c24acb732cfab5e56b2fa04fe8822caf9e62f954
jiyuujin LAB <jiyuujinunite@gmail.com>
2020-03-08 20:21:21 +0900

    use mdx-deck

---
+++ theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+    // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
+    fonts: {
+        body: 'Roboto, sans-serif',
+        monospace: '"Roboto Mono", monospace',
+    },
+    colors: {
+        text: 'black',
+        background: 'white',
+        primary: 'blue',
+    }
+}
